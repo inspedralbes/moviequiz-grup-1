359839ceb312cad30404deb2a0d8b9c93979f248
ASandraOrtiz <a20sanortveg@inspedralbes.cat>
2021-12-02 11:52:34 +0100

    footer

---
+++ front/PHP/footer.php
@@ -0,0 +1,9 @@
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <p class="grey-text text-lighten-4">© 2021 Copyright - Made by Pau Muñoz, Sandra Ortiz & Víctor Moreno</p>
+            </div>
+        </div>
+    </div>
+</footer>    
