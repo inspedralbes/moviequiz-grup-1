de80bf9dd1807a71edfff31e421814c7a0c7e663
ASandraOrtiz <a20sanortveg@inspedralbes.cat>
2021-12-02 13:46:50 +0100

    modals cercadors

--- front/JS/index.js
+++ front/JS/index.js
@@ -6,22 +6,28 @@ document.getElementById("btn_search").addEventListener("click", function (e) {
         res.json()).then(data => {
             var text_movie = "";
             for (var i = 0; i < 10; i++) {
+                var id = "movie" + i;
                 let data_movie = data.Search[i];
-                text_movie += imprimirpelis(data_movie);
+                text_movie += imprimirpelis(data_movie, id);
+                imprimirmodals(data_movie, id);
             }
             document.getElementById("resultat").innerHTML = text_movie;
+
+            /* Modals */
+            var instances = M.Modal.init(document.querySelectorAll(".modal"), {});
+
+
         })
 
 })
 
 /* Cards */
-function imprimirpelis(data_movie) {
+function imprimirpelis(data_movie, id) {
     var txt = `<div class="col s6 m4 l3" id="divpelis" class="divpelis">
                 <div class="card">
                     <div class="card-image">
                         <img class="poster" src="${data_movie.Poster}">
-                        <a class="btn-floating halfway-fab modal-trigger waves-effect waves-light red"
-                            data-target="sinopsis1"><i class="material-icons">add</i></a>
+                        <a class="btn-floating halfway-fab modal-trigger waves-effect waves-light red" href="#${id}"><i class="material-icons">add</i></a>
                     </div>
 
                     <div class="card-content card-title">
@@ -29,23 +35,28 @@ function imprimirpelis(data_movie) {
                         <span>${data_movie.Year}</span>
                     </div>
                 </div>
-                
-                <div id="modals" class="modal">
-                    <div class="modal-content">
-                        <h4 class="center-align cyan-text text-darken-3 ">${data_movie.Title}</h4>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="#" class="btn modal-close red "><i class="material-icons red">close</i></a>
-                    </div>
-                </div>
             </div>`;
     return txt;
 }
 
-/* Modals */
-document.addEventListener("DOMContentLoaded", function () {
-    var descripcio = document.querySelectorAll(".modal");
-    M.Modal.init(descripcio, {});
-});
+
+function imprimirmodals(data_movie, id) {
+    var txt = `<div id="${id}" class="modal">
+                    <div class="modal-content">
+                        <h4 class="center-align cyan-text text-darken-3 ">${data_movie.Title}</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#!" class="btn modal-close red "><i class="material-icons red">close</i></a>
+                    </div>
+                </div>`;
+    document.body.insertAdjacentHTML("beforeend", txt);
+}
+
+
+
+
+
+
+
 
 
