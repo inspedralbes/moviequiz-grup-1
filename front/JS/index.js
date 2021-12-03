0cd4c28fac77fea503456ad948035b3ffcdb8fea
pauchinpun <a20paumunoli@inspedralbes.cat>
2021-12-03 13:33:16 +0100

    Canvis modals de les pelicules al index.js

--- front/JS/index.js
+++ front/JS/index.js
@@ -2,34 +2,52 @@ $(document).ready(function() {
     $('input#input_text, textarea#textarea1').characterCounter();
 });
 
-document.getElementById("btn_search").addEventListener("click", function (e) {
+document.getElementById("btn_search").addEventListener("click", function(e) {
     var input = document.getElementById("search").value;
     let valoracio;
     fetch(`https://www.omdbapi.com/?apikey=5149518a&s=${input}&type=movie`).
-        then(res => res.json()).then(data => {
-            var text_movie = "";
-            for (var i = 0; i < 10; i++) {
-                var id = "movie" + i;
-                let data_movie = data.Search[i];
-                text_movie += imprimirPelisModals(data_movie, id);
-            }
+    then(res => res.json()).
+    then(data => {
+        var text_movie = "";
+        for (var i = 0; i < data.Search.length; i++) {
+            var idMovie = "movie" + i;
+            let data_movie = data.Search[i];
+            text_movie += imprimirPelisModals(data_movie, idMovie);
             document.getElementById("resultat").innerHTML = text_movie;
+        }
 
-            /* Inicialitzar modals */
-            var instances = M.Modal.init(document.querySelectorAll(".modal"), {});
+        document.getElementById("resultat").addEventListener("click", function(e) {
+            if (e.target.classList == "material-icons") {
+                id = e.target.parentElement.href.split("#")[1];
+                num = id.split("e")[1];
+                eliminarModals(data.Search);
+                document.getElementById(id).innerHTML = generarModal(data.Search[num]);
+                var instances = M.Modal.init(document.querySelectorAll(".modal"), {});
 
-            document.getElementById("formRadio").addEventListener("click", function(e){
-                valoracio = e.target.parentElement.querySelector("[name='valoracio']").value;
-            })
-
-
-            document.getElementById("formValoracio").addEventListener("click", function(e){
-                let favorito = (e.target.parentElement.querySelector("[name='fav']").value == "on") ? true : false ;
-                let comentario = e.target.parentElement.querySelector("#comentario").value;
-                console.log(valoracio + " " + favorito + " " + comentario);
-            });
-        })
+                document.getElementById("formRadio").addEventListener("click", function(e){
+                    valoracio = e.target.parentElement.querySelector("[name='valoracio']").value;
+                    console.log()
+                })
         
+        
+                document.getElementById("btn-guardar").addEventListener("click", function(e){
+                    let favorito = (e.target.parentElement.querySelector("[name='fav']").value == "on") ? true : false ;
+                    let comentario = e.target.parentElement.querySelector("#comentario").value;
+                    console.log(valoracio + " " + favorito + " " + comentario);
+                });
+            }
+        });
+
+        /* Inicialitzar modals */
+       
+
+
+
+
+
+
+    })
+
 })
 
 /* Cards */
@@ -46,15 +64,20 @@ function imprimirPelisModals(data_movie, id) {
                         <span>${data_movie.Year}</span>
                     </div>
                 </div>
-                <div id="${id}" class="modal">
-                    <div class="modal-content">
-                        <h4 class="center-align cyan-text text-darken-3 ">${data_movie.Title}</h4>
-                        <p>
+                <div id="${id}" class="modal"></div>
+            </div>`;
+    return txt;
+}
+
+function generarModal(nom) {
+    var modalHtml = `<div class="modal-content">
+                        <h4 class="center-align cyan-text text-darken-3 ">${nom.Title}</h4>
+                        <div>
                             <label>
                                 <input type="checkbox" id="fav" name="fav"/>
                                 <span>Afegir a pel·lícules favorites? </span>
                             </label>
-                        </p>
+                        </div>
 
                         <div class="input-field">
                             <textarea id="comentario" class="materialize-textarea" data-length="200"></textarea>
@@ -84,25 +107,16 @@ function imprimirPelisModals(data_movie, id) {
                             </label>
                         </div>
 
-                        <button id="formValoracio">Guardar</button>
+                        <button id="btn-guardar">Guardar</button>
                     </div>
                     <div class="modal-footer">
                         <a href="#!" class="btn modal-close red "><i class="material-icons red">close</i></a>
-                    </div>
-                </div>
-            </div>`;
-    return txt;
+                    </div>`;
+    return modalHtml;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function eliminarModals(data) {
+    for(let i = 0; i<data.length; i++){
+        document.getElementById("movie"+i).innerHTML = "";
+    }
+}
