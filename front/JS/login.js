0fec6fde82619e6e672794336a9a690573ba6e12
pauchinpun <a20paumunoli@inspedralbes.cat>
2021-12-10 10:32:35 +0100

    arreglar overflow

--- front/JS/login.js
+++ front/JS/login.js
@@ -57,8 +57,10 @@ document.getElementById("btn_entrar").addEventListener("click", function () {
             console.log("hola");
             document.getElementById("btn_login").classList.add("oculto");
             document.getElementById("a").innerHTML = codigoHTMLuser(data);
+            
             var instance = M.Modal.init(document.querySelector(".login"), {});
             instance.close();
+            document.body.style.removeProperty("overflow");
 
         } else {
             console.log("adios");
