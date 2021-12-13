a17090b86095e6e82b9a2ac3a6fa08f8a5fc00a5
ASandraOrtiz <a20sanortveg@inspedralbes.cat>
2021-12-13 12:27:44 +0100

    Pasar dades valoració peli BD (part 1, cal acabar amb insert)

--- front/PHP/controller_MQ.php
+++ front/PHP/controller_MQ.php
@@ -1,74 +1,89 @@
 <?php
 
-require_once('model_MQ.php');
-require_once('view_MQ.php');
-class controller
-{
+    require_once('model_MQ.php');
+    require_once('view_MQ.php');
 
-    //rutes o esdeveniments possibles
-    //view1: nom i edat
-    //view2: nom i alçada
-    private $peticions = array('login', 'signup');
-
-    
-    public function handler()
+    class controller
     {
-        $usuari = new usuari();
-        $pelicula = new pelicula();
-        $valoracio_pelicula = new valoracio_pelicula();
-        $partida = new partida();
-        $partida_jugada = new partida_jugada();
-        $vista = new view();
+        //rutes o esdeveniments possibles
+        //view1: nom i edat
+        //view2: nom i alçada
+        private $peticions = array('login', 'signup', 'valoracio');
 
-        // Què em demanen?
-        $event = 'inici';
+        public function handler()
+        {
+            $usuari = new usuari();
+            $pelicula = new pelicula();
+            $valoracio_pelicula = new valoracio_pelicula();
+            $partida = new partida();
+            $partida_jugada = new partida_jugada();
+            $vista = new view();
 
-        $uri = $_SERVER['REQUEST_URI'];
-        echo $uri;
+            // Què em demanen?
+            $event = 'inici';
 
-        foreach ($this->peticions as $peticio) {
-            if (strpos($uri, $peticio) == true) {
-                $event = $peticio;
+            $uri = $_SERVER['REQUEST_URI'];
+            //echo $uri;
+
+            foreach ($this->peticions as $peticio) {
+                if (strpos($uri, $peticio) == true) {
+                    $event = $peticio;
+                }
+            }
+
+            switch ($event) {
+                case 'signup':
+                    $dades = $this->recollirDadesPost();
+                    echo $usuari->introduirUsuari($dades);
+                    break;
+
+                case 'valoracio':
+                    $dades = $this->recollirDadesPost();
+                    $valoracio_pelicula->afegirValoracioPeli($dades);
+                    echo json_encode($dades);
+                    break;
+                
+                case 'login':
+                    $resposta = $this->login();
+                    $res = json_encode($resposta);
+                    echo $res;
+                    break;
             }
         }
 
-        switch ($event) {
-            case 'view1':
-                $dades = $usuari->selectAll(array("nom", "edat"));
-                break;
+        private function recollirDadesPost()
+        {
+            $dadesForm = false;
+            if (isset($_POST["signup"])) {
+                $dadesForm = array(
+                    'usuari' => $_POST["user"],
+                    'contrasenya' => $_POST["password"],
+                    'email' => $_POST["email"],
+                    'nom' => $_POST['nom'],
+                    'cognoms' => $_POST['cognom']
+                );
+            } else if (isset($_POST['usuari'])) {
+                $dadesForm = array(
+                    'usuari' => $_POST['usuari'],
+                    'contrasenya' => $_POST['pwd']
+                );
+            }else if (isset($_POST['valoracio'])){
+                $dadesForm = array(
+                    'valoracio' => $_POST["valoracio"],
+                    'favorit' => $_POST["favorit"],
+                    'comentari' => $_POST["comentari"]
+                );
+            }
+            return $dadesForm;
+        }
 
-            case 'signup':
-                $dades = $this->recollirDadesPost();
-                echo $usuari->introduirUsuari($dades);
-                break;
+        public function login(){
+            $usuari_login = new usuari();
+            $dadesPOST = $this->recollirDadesPost();
+            $resposta = $usuari_login->comprovarLogin($dadesPOST);
+            return $resposta;
         }
     }
 
-    private function recollirDadesPost()
-    {
-        $dadesForm = false;
-        if (isset($_POST["signup"])) {
-            $dadesForm = array(
-                'usuari' => $_POST["user"],
-                'contrasenya' => $_POST["password"],
-                'email' => $_POST["email"],
-                'nom' => $_POST['nom'],
-                'cognoms' => $_POST['cognom']
-            );
-        } else if (isset($_POST['usuari'])) {
-            $dadesForm = array(
-                'usuari' => $_POST['usuari'],
-                'contrasenya' => $_POST['pwd']
-            );
-        }
-
-        return $dadesForm;
-    }
-
-    public function login(){
-        $usuari_login = new usuari();
-        $dadesPOST = $this->recollirDadesPost();
-        $resposta = $usuari_login->comprovarLogin($dadesPOST);
-        return $resposta;
-    }
-}
+    $controller = new controller();
+    $controller->handler();
--- front/PHP/model_MQ.php
+++ front/PHP/model_MQ.php
@@ -49,19 +49,19 @@ class usuari extends BD_MovieQuiz
                 $this->message  = "Usuari introduït";
             } else $this->message = "L'usuari ja existeix";
         } else $this->message = "Usuari no introduït";
-        
+
         return $this->message;
     }
 
-    public function comprovarLogin($login_data = array()){
+    public function comprovarLogin($login_data = array())
+    {
         $this->query = "SELECT * FROM usuari WHERE user = '" . $login_data['usuari'] . "'";
         $this->get_results_from_query();
 
-        if($this->rows != null && $this->rows[0]["passw"] == $login_data['contrasenya']){
-            $res = array('exito'=>true, 'nombre'=>$this->rows[0]["nom"], 'imagen'=>'https://randomuser.me/api/portraits/men/23.jpg'); ;  
-        }
-        else {
-            $res = array('exito'=>false);
+        if ($this->rows != null && $this->rows[0]["passw"] == $login_data['contrasenya']) {
+            $res = array('exito' => true, 'nombre' => $this->rows[0]["nom"], 'imagen' => 'https://randomuser.me/api/portraits/men/23.jpg', 'puntuacion'=>$this->rows[0]["punts"]);;
+        } else {
+            $res = array('exito' => false);
         }
         return $res;
     }
@@ -293,52 +293,29 @@ class valoracio_pelicula extends BD_MovieQuiz
 
     //select dels camps passats de tots els registres
     //stored in $rows property
-    public function selectAll($fields = array())
-    {
-        $this->query = "SELECT ";
-        $firstField = true;
-        for ($i = 0; $i < count($fields); $i++) {
-            if ($firstField) {
-                $this->query .= $fields[$i];
-                $firstField = false;
-            } else $this->query .= ", " . $fields[$i];
-        }
-        $this->query .= " FROM persones";
-        $this->get_results_from_query();
-        return $this->rows;
-    }
 
-    public function select($nom = "")
+    public function afegirValoracioPeli($dadesvaloracio = array())
     {
-        $this->query = "SELECT ";
-        $this->get_results_from_query();
-        return $this->rows;
-    }
-
-
-    public function insert($user_data = array())
-    {
-        /*
-        if (array_key_exists("nom", $user_data)) {
-            $this->select($user_data["nom"]);
-            if (!isset($this->rows[0]['nom'])) {
-                foreach ($user_data as $campo => $c) {
-                    $$campo = $c;
+        if (array_key_exists("valoracio", $dadesvaloracio)) {
+            $this->dadesValoracio($user_data["usuari"]);
+            if (!isset($this->rows[0]['user'])) {
+                foreach ($user_data as $campo => $dato) {
+                    $$campo = $dato;
                 }
-                $this->query = "INSERT INTO persones(id, nom, edat, alcada) VALUES (NULL, '" . $nom . "', '" . $edat . "', '" . $alcada . "')";
+                $this->query = "INSERT INTO usuari(idUsuari, nom, cognoms, email, user, passw, imatge, punts) VALUES (NULL, '$nom', '$cognoms', '$email', '$usuari', '$contrasenya', NULL, 0)";
                 $this->execute_single_query();
                 $this->message  = "Usuari introduït";
             } else $this->message = "L'usuari ja existeix";
-        } else $this->message = "Usuari no introduït";*/
+        } else $this->message = "Usuari no introduït";
+
+        return $this->message;
+        
     }
 
-    public function update($userData = array())
+    public function dadesValoracio($user = "")
     {
-    }
-
-    public function delete($nom = "")
-    {
+        $this->query = "SELECT * FROM usuari WHERE user = '$user'";
+        $this->get_results_from_query();
+        return $this->rows;
     }
 }
-
-?>
