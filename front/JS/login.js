2afe9cd8ad88a2a29f0581e363637d133f60297b
pauchinpun <a20paumunoli@inspedralbes.cat>
2021-12-13 12:37:14 +0100

    + Cambios

--- front/JS/login.js
+++ front/JS/login.js
@@ -3,43 +3,6 @@ document.getElementById("btn_login").addEventListener("click", function (e) {
     var instances = M.Modal.init(document.querySelector(".login"), {});
 })
 
-
-// LOGIN
-
-let u = document.getElementById("usuari").value;
-let c = document.getElementById("pwd").value;
-
-const datosLogin = new FormData();
-datosLogin.append("usuari", u);
-datosLogin.append("pwd", c);
-
-/*fetch(`../PHP/header.phh`, {
-    method: "POST",
-    body: datosLogin
-})
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        console.log(data);
-
-        // Usuari
-        if (data.exito == true) {
-            document.getdocument.getElementById("btn-guardar").classList.remove("disabled");
-            document.getElementById("diverror").classList.remove("oculto");
-            document.getElementById("resultat").classList.remove("oculto");
-
-        }// NO usuari
-        else {
-            document.getElementById("btn-guardar").classList.add("disabled");
-            document.getElementById("diverror").classList.add("oculto");
-        }
-
-    });*/
-
-
-
 document.getElementById("btn_entrar").addEventListener("click", function () {
     let usu = document.getElementById("usuari").value;
     let pwd = document.getElementById("pwd").value;
@@ -59,7 +22,7 @@ document.getElementById("btn_entrar").addEventListener("click", function () {
             document.getElementById("btn_login").classList.add("oculto");
             document.getElementById("carrousel-fotos").classList.add("oculto");
             document.getElementById("carrousel-titol").classList.add("oculto");
-            document.getElementById("apartadoCarrousel").insertAdjacentHTML("aftersbegin", loginHTML)
+            document.getElementById("apartadoCarrousel").insertAdjacentHTML("afterbegin", loginHTML);
             document.getElementById("info-usuari").innerHTML = codigoHTMLuser(data);
         } else {
             console.log("adios");
