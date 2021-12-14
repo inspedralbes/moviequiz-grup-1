dffe11a02b62ab7a1d56ab5e27048eddcf389416
a20paumunoli <a20paumunoli@inspedralbes.cat>
2021-12-14 23:55:51 +0100

    Canvis footer & header (php i css)

--- front/PHP/footer.php
+++ front/PHP/footer.php
@@ -1,9 +1,20 @@
 <footer class="page-footer deep-purple">
     <div class="container">
-        <div class="row center-align">
-            <div class="col center-align">
-                <p class="center-align grey-text text-lighten-4 ">© 2021 Copyright - Made by Pau Muñoz, Sandra Ortiz & Víctor Moreno</p>
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Informació</h5>
+                <p class="grey-text text-lighten-4">Tot el contingut prové de l'API de OMDb, que és un servei web RESTful per obtenir informació sobre pel·lícules</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Sobre nosaltres</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Contactans</a></li>
+                </ul>
             </div>
         </div>
     </div>
-</footer>    
+    <div class="footer footer-copyright">
+        <p class="grey-text text-lighten-4">© 2021 Copyright - Made by Pau Muñoz, Sandra Ortiz & Víctor Moreno</p>
+    </div>
+</footer>
--- front/PHP/header.php
+++ front/PHP/header.php
@@ -1,43 +1,43 @@
 <nav>
     <div id="nav" class="nav-wrapper deep-purple">
-        <a href="index.php" class="brand-logo"><img src="../IMG/logo2.png" width="50px"></a>
-        <ul class="right hide-on-med-and-down">
+        <a href="index.php">
+            <img src="../IMG/logo2.png" class="logo" width="50px">
+        </a>
+        <ul id="nav-mobile" class="right">
             <li><a id="btn_login" href="#login" class="modal-trigger waves-effect waves-light btn">LOGIN</a></li>
             <li id="resultat_header"></li>
         </ul>
     </div>
-
-    <!-- LOGIN MODAL -->
-
-        <div id="login" class=" login modal">
-        <div class="modal-content">
-            <div class="row center">
-                <h5 class="red-text darken-1">Inicia Sessió</h5>
-                <form class="col s4 offset-m3 l4 offset-l4">
-                    <div class="input-field">
-                        <i class="material-icons prefix">person</i>
-                        <label for="usuari">Usuari</label>
-                        <input type="text" id="usuari" name="usuari">
-                    </div>
-
-                    <div class="input-field">
-                        <i class="material-icons prefix">key</i>
-                        <label for="pwd">Contrasenya</label>
-                        <input type="password" id="pwd" name="pwd">
-                    </div>
-                </form>
-            </div>
-
-            <div class="row center center-align">
-                <a id="btn_entrar" class="waves-effect waves-light btn-small">Entrar</a>
-                <p class="grey-text link">No ets usuari?,<a href="signup.php"> registrat</a></p>
-            </div>
-        </div>
-
-        <div class="modal-footer">
-            <a href="#!" class="btn modal-close red ">Tancar</a>
-        </div>
-        </div>
-
-
 </nav>
+
+
+<!-- LOGIN MODAL -->
+<div id="login" class=" login modal">
+    <div class="modal-content">
+        <div class="row center">
+            <h5 class="red-text darken-1">Inicia Sessió</h5>
+            <form class="col s4 offset-m3 l4 offset-l4">
+                <div class="input-field">
+                    <i class="material-icons prefix">person</i>
+                    <label for="usuari">Usuari</label>
+                    <input type="text" id="usuari" name="usuari">
+                </div>
+
+                <div class="input-field">
+                    <i class="material-icons prefix">key</i>
+                    <label for="pwd">Contrasenya</label>
+                    <input type="password" id="pwd" name="pwd">
+                </div>
+            </form>
+        </div>
+
+        <div class="row center center-align">
+            <a id="btn_entrar" class="waves-effect waves-light btn-small">Entrar</a>
+            <p class="grey-text link">No ets usuari?,<a href="signup.php"> registrat</a></p>
+        </div>
+    </div>
+
+    <div class="modal-footer">
+        <a href="#!" class="btn modal-close red ">Tancar</a>
+    </div>
+</div>
