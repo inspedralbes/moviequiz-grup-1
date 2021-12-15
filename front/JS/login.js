65233f96da087af4beaf3267011b20db0e8ac8e3
ASandraOrtiz <a20sanortveg@inspedralbes.cat>
2021-12-15 09:08:25 +0100

    canviar imatge al editar l'usuari

--- front/JS/login.js
+++ front/JS/login.js
@@ -1,9 +1,9 @@
 /* Inicialitzar modals */
-document.getElementById("btn_login").addEventListener("click", function(e) {
+document.getElementById("btn_login").addEventListener("click", function (e) {
     var instances = M.Modal.init(document.querySelector(".login"), {});
 })
 
-document.getElementById("btn_entrar").addEventListener("click", function() {
+document.getElementById("btn_entrar").addEventListener("click", function () {
     let usu = document.getElementById("usuari").value;
     let pwd = document.getElementById("pwd").value;
 
@@ -28,6 +28,8 @@ document.getElementById("btn_entrar").addEventListener("click", function() {
             document.getElementById("btn_edit").addEventListener("click", function (e) {
                 document.getElementById("nom_us").removeAttribute("disabled");
                 document.getElementById("email_us").removeAttribute("disabled");
+                document.getElementById("canviar_img_usr").classList.remove("oculto");
+                document.getElementById("img_usr_label").classList.remove("oculto");
                 document.getElementById("btn_save").classList.remove("oculto");
                 document.getElementById("btn_edit").classList.add("oculto");
             })
@@ -35,6 +37,8 @@ document.getElementById("btn_entrar").addEventListener("click", function() {
             document.getElementById("btn_save").addEventListener("click", function (e) {
                 document.getElementById("nom_us").setAttribute("disabled", "");
                 document.getElementById("email_us").setAttribute("disabled", "");
+                document.getElementById("canviar_img_usr").classList.add("oculto");
+                document.getElementById("img_usr_label").classList.add("oculto");
                 document.getElementById("btn_edit").classList.remove("oculto");
                 document.getElementById("btn_save").classList.add("oculto");
             })
@@ -50,7 +54,7 @@ document.getElementById("btn_entrar").addEventListener("click", function() {
 
 function codigoHTMLheaderuser(datos) {
     let text = `<li class="usuario_header">${datos.usuari}</li>
-                <li><img class="img_header" src="${datos.imagen}"></li>
+                <li><img class="img_header circle responsive-img" src="${datos.imagen}"></li>
                 <li><a id="btn_logout" href="logout.php" class="modal-trigger waves-effect waves-light btn">LOGOUT</a></li>`;
     return text;
 }
@@ -61,7 +65,7 @@ function codigoHTMLuser(datos) {
     let text = `<div class="row">
                     <div class="col s3 m3 l3 centrar">
                         <h3>¡Hola ${datos.nombre}!</h3>
-                        <img src="${datos.imagen}">
+                        <img src="${datos.imagen}" class="circle responsive-img">
                     </div>
 
                     <div class="dades_usuari center-align>
@@ -76,6 +80,17 @@ function codigoHTMLuser(datos) {
                             
                                 <label class="white_font" for="puntuacio">Karma</label>
                                 <input disabled id="puntuacio_us" type="text" class="white_font" value="${datos.puntuacion}">
+
+                                <label class="white_font oculto" id="img_usr_label" for="imatge">Imatge</label>
+
+                                <div class="file-field input-field oculto" id="canviar_img_usr">
+                                    <div class="btn-small">
+                                        <input type="file" multiple accept="image/*"><i class="material-icons">insert_photo</i></input>
+                                    </div>
+                                    <div class="file-path-wrapper">
+                                        <input class="file-path" type="text" class="white_font">
+                                    </div>
+                                </div>
 
                                 <input id="alias" type="hidden" value="${datos.usuari}">
                             </div>
